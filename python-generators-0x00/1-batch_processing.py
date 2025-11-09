c1b33e818065b9fd0a91e3dc1ddedc83d250672f
ILYAS EL ATTAR <ilyasseelattar@gmail.com>
2025-11-09 20:08:47 +0000

    fix

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-import mysql.connector
-from seed import connect_to_prodev
+import seed
 
 def stream_users_in_batches(batch_size):
-    """Generator function that fetches rows from user_data table in batches"""
-    connection = connect_to_prodev()
-    if connection:
-        cursor = connection.cursor(dictionary=True)
-        offset = 0
-        while True:
-            cursor.execute(
-                "SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset)
-            )
-            batch = cursor.fetchall()
-            if not batch:
-                break
-            yield batch
-            offset += batch_size
-        cursor.close()
-        connection.close()
+    """
+    Generator that yields batches of users from the database
+    """
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    offset = 0
+
+    while True:
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s;", (batch_size, offset))
+        batch = cursor.fetchall()
+        if not batch:
+            break
+        yield batch
+        offset += batch_size
+
+    cursor.close()
+    connection.close()
+
 
 def batch_processing(batch_size):
-    """Process each batch and print users over age 25"""
+    """
+    Process each batch and print users over the age of 25
+    """
     for batch in stream_users_in_batches(batch_size):
-        for user in batch:
+        for user in batch:  # second loop
             if user['age'] > 25:
                 print(user)
