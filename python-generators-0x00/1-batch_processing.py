184efc3b612aa6b5553364a9a81ddf20a786489f
ILYAS EL ATTAR <ilyasseelattar@gmail.com>
2025-11-09 20:16:18 +0000

    fix

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
+"""
+Batch processing of users.
+"""
 import seed
 
+
 def stream_users_in_batches(batch_size):
-    """Generator to fetch user_data rows in batches"""
+    """Yield users in batches of given size."""
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
 
-    # Execute the query ONCE
-    cursor.execute("SELECT * FROM user_data;")
-
+    cursor.execute("SELECT * FROM user_data")
     while True:
-        count = 0
-        while count < batch_size:
-            row = cursor.fetchone()  # fetch one row at a time
-            if row is None:
-                break
-            yield row
-            count += 1
-        if count == 0:  # no more rows
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
             break
+        yield rows
 
     cursor.close()
     connection.close()
 
 
 def batch_processing(batch_size):
-    """Process each batch and print users over age 25"""
-    for user in stream_users_in_batches(batch_size):
-        if user['age'] > 25:
-            print(user)
-
+    """
+    Process each batch and filter users older than 25.
+    """
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if int(user['age']) > 25:
+                print(user)
